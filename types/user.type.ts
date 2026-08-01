d7333f4d540afd6cd5fb084438b10fcf91b68444
Rocky Chowdhury <rocky20809@gmail.com>
2026-08-02 00:07:51 +0600

    user type added

---
+++ types/user.type.ts
@@ -0,0 +1,23 @@
+import { UserRole } from "./auth.type";
+
+export type UserProfile = {
+  id: string;
+  userId: string;
+  fullName: string;
+  bio: string | null;
+  avatarUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  phone: string;
+  role: UserRole;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  profile?: UserProfile | null;
+};
